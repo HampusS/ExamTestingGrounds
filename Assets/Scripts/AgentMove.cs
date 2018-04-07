381bb53b6fec9ff9a99c97d398e8da8c18b4879b
Hampus Strömsholm <supmahampus@gmail.com>
2018-04-07 20:48:50 +0200

    Started a swarm enemy

---
+++ Assets/Scripts/AgentMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    NavMeshAgent navMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetDestination()
+    {
+        navMesh.SetDestination(target.position);
+    }
+}
