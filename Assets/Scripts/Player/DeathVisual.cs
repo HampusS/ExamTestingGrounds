cb31b2bb29234cfcfe17f6f89c34a047f9189e61
Tomdozz <tom_1995@msn.com>
2018-04-13 10:17:54 +0200

    Added respawn function, use RespawnProperties[lastFailes]

---
+++ Assets/Scripts/Player/DeathVisual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class DeathVisual : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Dead()
+    {
+        anim.SetTrigger("Dead");
+    }
+    public void Alive()
+    {
+        anim.SetTrigger("Alive");
+    }
+}
