72a0a2a0158e31d91b7eb0907145036e064e92d6
HampusS <supmahampus@gmail.com>
2018-04-17 19:42:23 +0200

    Added trail renderer

--- Assets/Scripts/Player/Grenade.cs
+++ Assets/Scripts/Player/Grenade.cs
@@ -4,58 +4,61 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    public float delay = 3;
+    public float lifeTime = 3;
     public float blastRadius = 5;
     public float force = 700;
     public GameObject effect;
+
+    Rigidbody rb;
 
     bool exploded;
     float countdown;
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Instantiate(effect, transform.position, transform.rotation);
-        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
-        foreach (Collider nearObj in colliders)
-        {
-            Rigidbody rb = nearObj.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddExplosionForce(force, transform.position, blastRadius);
-            }
-        }
-
-        Destroy(gameObject);
+        Explode();
     }
 
     // Use this for initialization
     void Start()
     {
-        countdown = delay;
+        countdown = lifeTime;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        countdown -= Time.deltaTime;
-        if (countdown <= 0 && !exploded)
-            Explode();
+        if (!exploded)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+                Explode();
+            transform.rotation = Quaternion.LookRotation(transform.forward);
+        }
     }
 
     void Explode()
     {
-        exploded = true;
-        //Instantiate(effect, transform.position, transform.rotation);
-        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
-        foreach (Collider nearObj in colliders)
+        if (!exploded)
         {
-            Rigidbody rb = nearObj.GetComponent<Rigidbody>();
-            if (rb != null)
+            exploded = true;
+            //Instantiate(effect, transform.position, transform.rotation);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+            foreach (Collider nearObj in colliders)
             {
-                rb.AddExplosionForce(force, transform.position, blastRadius);
+                Rigidbody rb = nearObj.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, transform.position, blastRadius);
+                }
             }
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(gameObject, lifeTime);
         }
-
-        Destroy(gameObject);
     }
 }
