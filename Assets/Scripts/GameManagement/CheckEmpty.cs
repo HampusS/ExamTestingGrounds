cee994ecf108653e093cd19e53647b3c009b5a85
Tomdozz <tom_1995@msn.com>
2018-04-17 17:53:00 +0200

    almost doen

---
+++ Assets/Scripts/GameManagement/CheckEmpty.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckEmpty : MonoBehaviour
+{
+    int count;
+    public Light spotlight;
+    public Color color0;
+    public Color color1;
+    public float lerpFloat = 13;
+    void ChangeLightColor()
+    {
+        if (isEmpty())// && spotlight.color != color1)
+        {
+            spotlight.color = color1;//Color.Lerp(color0, color1, Time.deltaTime*HampusFloat);
+        }
+    }
+    bool isEmpty()
+    {
+        if (count <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GoalCubeTag")
+        {
+            count++;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "GoalCubeTag")
+        {
+            count--;
+        }
+        Debug.Log(count);
+        if (isEmpty())
+        {
+            Invoke("ChangeLightColor", 3);
+        }
+    }
+
+}
