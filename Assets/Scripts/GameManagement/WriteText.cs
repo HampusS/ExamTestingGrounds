a251e3cee68eb78f5a2af2cda4cedec153a73c44
Tomdozz <tom_1995@msn.com>
2018-04-19 21:37:36 +0200

    fixed some fps drops, started working on instructions

---
+++ Assets/Scripts/GameManagement/WriteText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriteText : MonoBehaviour {
+
+    public string text;
+    string meshString = "";
+    TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+        textMesh = GetComponent<TextMesh>();
+        Debug.Log(text[3]);
+        TextAnim(text);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    void TextAnim(string inputText)
+    {
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            //new WaitForSeconds(544.5f);
+            Invoke("AddLetter(" + inputText[i] + ")", 50);
+        }
+    }
+    void AddLetter(char letter)
+    {
+            textMesh.text = textMesh.text + letter;
+
+    }
+}
